5b92bb961fe2e5ba11ae1578c0029852a96b5865
knakamatsu <maikuro0303@gmail.com>
2018-05-20 03:21:32 +0900

    認証サーバーをspring cloudに切り替え / 名前をWebfront(BFF)サーバーに変更

--- web-front/src/main/java/com/Application.java
+++ web-front/src/main/java/com/Application.java
@@ -19,9 +19,13 @@ package com;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
 @EnableWebSecurity
 @EnableOAuth2Sso
 public class Application {
