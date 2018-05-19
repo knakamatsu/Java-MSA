c9c39162592639de6da20846b55345a83df57f5a
knakamatsu <maikuro0303@gmail.com>
2018-05-20 04:24:46 +0900

    fileManagerをspring bootに切り替え / sso認証出来るように変更 / webfrontのAPI転送時のタイムアウトを10秒に変更（github認証時にタイムアウトする）

--- fileManager/src/main/java/com/Application.java
+++ fileManager/src/main/java/com/Application.java
@@ -16,17 +16,20 @@
 
 package com;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
+@EnableEurekaClient
+@EnableOAuth2Client
+@EnableWebSecurity
+@EnableResourceServer
 @SpringBootApplication
-public class Application extends SpringBootServletInitializer {
-
+public class Application {
 	public static void main(String[] args) {
-		new Application()
-				.configure(new SpringApplicationBuilder(Application.class))
-				.run(args);
+		SpringApplication.run(Application.class, args);
 	}
-
 }
--- web-front/src/main/java/com/Application.java
+++ web-front/src/main/java/com/Application.java
@@ -23,11 +23,11 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
-@SpringBootApplication
 @EnableEurekaClient
 @EnableZuulProxy
 @EnableWebSecurity
 @EnableOAuth2Sso
+@SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
