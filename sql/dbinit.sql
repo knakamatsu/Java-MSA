a9785640331c78060f9924e825aa0f0b3491f4e1
knakamatsu <maikuro0303@gmail.com>
2018-05-21 23:37:54 +0900

    sqlファイル修正

--- sql/dbinit.sql
+++ sql/dbinit.sql
@@ -1,32 +1,14 @@
-CREATE DATABASE msa
-    WITH
-    OWNER = postgres
-    ENCODING = 'UTF8'
-    LC_COLLATE = 'Japanese_Japan.932'
-    LC_CTYPE = 'Japanese_Japan.932'
-    TABLESPACE = pg_default
-    CONNECTION LIMIT = -1;
+CREATE DATABASE msa;
 
 CREATE SCHEMA usermanagement
     AUTHORIZATION postgres;
 
--- DROP TABLE usermanagement.users;
-
-CREATE TABLE usermanagement.users
-(
-    id integer NOT NULL DEFAULT nextval('usermanagement.users_id_seq'::regclass),
-    name character varying COLLATE pg_catalog."default",
-    address character varying COLLATE pg_catalog."default",
-    phonenumber character varying COLLATE pg_catalog."default",
-    CONSTRAINT users_pkey PRIMARY KEY (id)
-)
-WITH (
-    OIDS = FALSE
-)
-TABLESPACE pg_default;
-
-ALTER TABLE usermanagement.users
-    OWNER to postgres;
+CREATE TABLE usermanagement.users (
+	id SERIAL PRIMARY KEY
+	, name VARCHAR
+	, address VARCHAR
+	, phonenumber VARCHAR
+);
 
 INSERT INTO usermanagement.users ( name, address, phonenumber ) VALUES
  ('KeiNakamatsu','和歌山県田辺市どこか','09011003300')
